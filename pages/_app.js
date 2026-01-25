084c2a57a4257f2ff3be99e779f2962682950f5c
Vaibhav Baldwa <vaibhavbaldwa@Vaibhavs-MacBook-Pro.local>
2026-01-25 21:41:20 +0530

    Add Google Search Console verification

--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,7 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
+        <meta name="google-site-verification" content="Z9xTMB9QvR1pmVmtHWQ1nWapJY13OxbLeKW141IE7E4" />
         <script src="https://cdn.tailwindcss.com"></script>
       </Head>
       <Component {...pageProps} />
