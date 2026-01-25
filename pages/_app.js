6b8f139c856e21bb1d78cb6e9af93b3a91bbdd04
Vaibhav Baldwa <vaibhavbaldwa@Vaibhavs-MacBook-Pro.local>
2026-01-25 23:10:21 +0530

    Add Google AdSense code

--- pages/_app.js
+++ pages/_app.js
@@ -6,6 +6,8 @@ export default function App({ Component, pageProps }) {
     <>
       <Head>
         <meta name="google-site-verification" content="Z9xTMB9QvR1pmVmtHWQ1nWapJY13OxbLeKW141IE7E4" />
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5099977081953527"
+     crossorigin="anonymous"></script>
         <script src="https://cdn.tailwindcss.com"></script>
       </Head>
       <Component {...pageProps} />
