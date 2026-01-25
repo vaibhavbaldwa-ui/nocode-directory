43d36887680d7fe4cf14379f5f5a4d2bf4a8a29f
Vaibhav Baldwa <vaibhavbaldwa@Vaibhavs-MacBook-Pro.local>
2026-01-25 23:29:19 +0530

    Update Google verification for custom domain

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,7 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <meta name="google-site-verification" content="Z9xTMB9QvR1pmVmtHWQ1nWapJY13OxbLeKW141IE7E4" />
+        <meta name="google-site-verification" content="izbrIsILGCMw6QV1lkfHfWBh_GL50Q2JzHwH2oOmlmw" />
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5099977081953527"
      crossorigin="anonymous"></script>
         <script src="https://cdn.tailwindcss.com"></script>
