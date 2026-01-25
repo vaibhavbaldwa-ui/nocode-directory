1a65c6ac25123e4e60f0ffd70e2335161403e43d
Vaibhav Baldwa <vaibhavbaldwa@Vaibhavs-MacBook-Pro.local>
2026-01-25 23:59:09 +0530

    Update Google AdSense code with custom domain

--- pages/_app.js
+++ pages/_app.js
@@ -8,6 +8,8 @@ export default function App({ Component, pageProps }) {
         <meta name="google-site-verification" content="izbrIsILGCMw6QV1lkfHfWBh_GL50Q2JzHwH2oOmlmw" />
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5099977081953527"
      crossorigin="anonymous"></script>
+     <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5099977081953527"
+     crossorigin="anonymous"></script>
         <script src="https://cdn.tailwindcss.com"></script>
       </Head>
       <Component {...pageProps} />
