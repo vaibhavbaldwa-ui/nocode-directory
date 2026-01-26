c11f9718d82be54ae36b8d54da937a58b8b6b398
Vaibhav Baldwa <vaibhavbaldwa@Vaibhavs-MacBook-Pro.local>
2026-01-26 18:23:02 +0530

    Fix mobile scroll, add analytics_v3

--- pages/_app.js
+++ pages/_app.js
@@ -6,25 +6,26 @@ export default function App({ Component, pageProps }) {
     <>
       <Head>
         <meta name="google-site-verification" content="izbrIsILGCMw6QV1lkfHfWBh_GL50Q2JzHwH2oOmlmw" />
+        
+        {/* AdSense - Only once! */}
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5099977081953527"
      crossorigin="anonymous"></script>
-     <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5099977081953527"
-     crossorigin="anonymous"></script>
+        
+        {/* Google Analytics */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-FB0V1ES4MQ"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-FB0V1ES4MQ');
+            `,
+          }}
+        />
+        
         <script src="https://cdn.tailwindcss.com"></script>
       </Head>
-      {/* Google Analytics */}
-      <Script
-        src="https://www.googletagmanager.com/gtag/js?id=G-XXXXXXXXXX"
-        strategy="afterInteractive"
-      />
-      <Script id="google-analytics" strategy="afterInteractive">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'G-FB0V1ES4MQ');
-        `}
-      </Script>
       <Component {...pageProps} />
     </>
   )
